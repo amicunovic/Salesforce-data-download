1a8de16e7805ea1aa8170440d3b92590b826be31
amicunovic <45373569+amicunovic@users.noreply.github.com>
2018-11-28 14:38:33 +0100

    Add files via upload

--- SF3.py
+++ SF3.py
@@ -4,6 +4,9 @@ from salesforce_bulk.util import IteratorBytesIO
 import unicodecsv
 import pandas as pd
 import time
+import xlsxwriter
+
+print("This program is used to download all IBs from the system \n\n\nOnce all the data is downloaded you have two options: \n\n1. Write to existing file (Note that this program and that file have to be in the same place) \n2. Create a new file")
 
 list1 = []
 
@@ -26,7 +29,7 @@ for result in bulk.get_all_results_for_query_batch(batch):
 
 '''
 job = bulk.create_query_job("ChikPeaTOM__Install_Base__c", contentType='CSV')
-batch = bulk.query(job, "select Id,Name, Service_ID__c, Account_Name__c from ChikPeaTOM__Install_Base__c")
+batch = bulk.query(job, "select Id,Name, Service_ID__c, Account_Name__c, ChikPeaTOM__Status__c, ChikPeaTOM__Unit_MRC__c, Last_Billed_On__c, Plan_Name__c, Billing_Stop_Date__c from ChikPeaTOM__Install_Base__c")
 bulk.close_job(job)
 while not bulk.is_batch_done(batch):
     time.sleep(10)
@@ -39,8 +42,31 @@ for result in bulk.get_all_results_for_query_batch(batch):
     	dict1 = dict(row)
     	list1.append(dict1)
 df = pd.DataFrame(list1)
-df = df[['Id', 'Name', 'Service_ID__c', 'Account_Name__c', 'Id']]
+df = df[['Id', 'Name', 'Service_ID__c', 'Account_Name__c', 'Id', 'ChikPeaTOM__Status__c', 'ChikPeaTOM__Unit_MRC__c', 'Last_Billed_On__c', 'Plan_Name__c', 'Billing_Stop_Date__c']]
 
-report_name_excel = str(input('Please enter the report name '))
-df.to_csv(report_name_excel+'.csv',index=False)
+answerme = input("\nPlease choose one of the options explained above, type only the number of the option ")
 
+
+
+if answerme == '2':
+	exfile = input("\nName the new file ")
+	sheet_name = input("\nEnter the sheet name ")
+	writer = pd.ExcelWriter(exfile+'.xlsx', engine='xlsxwriter')
+	df.to_excel(writer, sheet_name=sheet_name, index=False)
+	workbook  = writer.book
+	worksheet = writer.sheets[sheet_name]
+	format1 = workbook.add_format({'num_format': '#,##0.00'})
+	worksheet.set_column('G:G', None, format1)
+	writer.save()
+
+
+elif answerme == '1':
+	exfile = input("\nName the existing file (It has to be in xlsx fromat) ")
+	sheet_name = input("\nEnter the sheet name ")
+	writer = pd.ExcelWriter(exfile+'.xlsx', engine='xlsxwriter')
+	df.to_excel(writer, sheet_name=sheet_name, index=False)
+	workbook  = writer.book
+	worksheet = writer.sheets[sheet_name]
+	format1 = workbook.add_format({'num_format': '#,##0.00'})
+	worksheet.set_column('G:G', None, format1)
+	writer.save()
